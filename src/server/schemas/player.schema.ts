8f269a72f2e80b449970f1060afbeff9dd951a19
sirpuffin1 <angelcruz981@yahoo.com>
2021-12-07 17:50:43 -0800

    chore( remove user input, and update player- Schema)

--- src/server/schemas/player.schema.ts
+++ src/server/schemas/player.schema.ts
@@ -3,7 +3,7 @@ const {Schema, model} = mongoose;
 
 const playerSchema = new Schema<any>({
    socketId: String,
-   playerName: String,
+   playerName: {type: String, default: 'player'},
 });
 
 export const PlayerModel = model('Player', playerSchema);
