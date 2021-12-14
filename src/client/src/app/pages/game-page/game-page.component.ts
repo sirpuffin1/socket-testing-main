b3f9cfa24c1da3bd7b962804dedcc6b3893d0a65
sirpuffin1 <angelcruz981@yahoo.com>
2021-12-13 20:43:27 -0800

    feat(client): add username pop-up dialog, and implement socketService to send and receive socket messages

--- src/client/src/app/pages/game-page/game-page.component.ts
+++ src/client/src/app/pages/game-page/game-page.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { SocketioService } from 'src/app/services/socket.service';
+
+
+
+export interface DialogData {
+  name: string;
+}
 
 @Component({
   selector: 'app-game-page',
   templateUrl: './game-page.component.html',
   styleUrls: ['./game-page.component.scss']
 })
-export class GamePageComponent implements OnInit {
+export class GamePageComponent implements OnInit, OnDestroy {
+  name!: string;
+  newMessage!: string;
+  messageList: string[] = []
+  display: boolean = true;
+  username: string = ''
 
-  constructor() { }
-
+  constructor(private socketService: SocketioService, private cd: ChangeDetectorRef) { }
+  ngOnDestroy(): void {
+      console.log('destroy')
+  }
   ngOnInit(): void {
+    console.log('init')
+    this.socketService.getNewMessage().subscribe((message) => {
+      // this.messageList.push(message);
+      // this.cd.detectChanges();
+    })
+    this.socketService.getMessage().subscribe((message) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage() {
+      this.socketService.sendMessage(this.newMessage)
+      console.log(this.newMessage)
+      console.log(this.messageList)
+      this.newMessage = ''
+  }
+  showDialog() {
+    this.display = false;
   }
 
 }
+
+
